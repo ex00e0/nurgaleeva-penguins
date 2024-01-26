384db58ad32c3a0dd9be402044c2820281e18cd2
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-01-26 15:07:44 +0500

    добавлено добавление в бд

--- createNewValid.php
+++ createNewValid.php
@@ -6,17 +6,23 @@ $newHeadline = $_POST["newHeadline"];
 $newText = $_POST["newText"];
 $newImage = $_FILES["newImage"];
 $newCategory = $_POST["newCategory"];
-// var_dump(gettype($newHeadline) == "string");
-// if ($newHeadline == "" || $newText == "" || $newImage['size'] == 0 || $newCategory == "") {echo '<script> alert("Не все поля заполнены"); </script>';}
-// if (mb_strlen($newHeadline)>20) {echo '<script> alert("Слишком длинное название"); </script>';}
+function check ($con, $newImage, $newHeadline, $newText, $newCategory) {$result=0;
+    if ($newHeadline == "" || $newText == "" || $newImage['size'] == 0 || $newCategory == "") {echo '<script> alert("Не все поля заполнены"); </script>';}
+    else if (mb_strlen($newHeadline)>20) {echo '<script> alert("Слишком длинное название"); </script>';}
 
-//  if (gettype($newHeadline) == "string") {echo '<script> alert("тип данных заголовка верный"); </script>';} else {echo '<script> alert("Ошибка в типах данных текста заголовка"); </script>';}
+ else if (gettype($newHeadline) != "string") {echo '<script> alert("Ошибка в типах данных текста заголовка"); </script>';} 
 
-// if (gettype($newText) == "string") {echo '<script> alert("тип данных текста новости верный"); </script>';} else {echo '<script> alert("Ошибка в типах данных текста новости"); </script>';}
+else if (gettype($newText) != "string") {echo '<script> alert("Ошибка в типах данных текста новости"); </script>';} 
 
-// if ($newImage["type"] == "image/jpeg" ||  $newImage["type"] == "image/jpg" || $newImage["type"] == "image/png") {echo '<script> alert("Пришла картинка"); </script>';} else {echo '<script> alert("Пришла не картинка..."); </script>';}
-$insert = "INSERT INTO news (image, title, content, category_id) VALUES ('$newImage[name]', '$newHeadline', '$newText','$newCategory')";
-$result = mysqli_query($con, $insert);
+else if (substr($newImage["type"], 0, 5) != "image") {echo '<script> alert("Пришла не картинка"); </script>';} 
+
+else {$insert = "INSERT INTO news (image, title, content, category_id) VALUES ('$newImage[name]', '$newHeadline', '$newText','$newCategory')";
+    $result = mysqli_query($con, $insert);}  
+return $result;   }
+if (check($con, $newImage, $newHeadline, $newText, $newCategory)) {echo "Отправлено в БД";}
+else {echo "Не отправлено в БД. Перепроверьте введенные данные.";}
+
+
 
 //создание строки с запросом на вставку в таблицу news из 3 значений Image title description
 ?>
